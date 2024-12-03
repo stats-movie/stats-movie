373538f3bc28599124bee8caac19524d4fe6a953
Gabriel Martins Rocha <gmrgabriel1@gmail.com>
2024-12-02 21:50:20 -0300

    Adicionando template para erro 404 e começando session

--- stats-movie/app.py
+++ stats-movie/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "rochakkj."
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route("/")
 def home():
@@ -10,13 +12,36 @@ def home():
 @app.route("/cadastro", methods = ["POST", "GET"])
 def cadastro():
     if request.method == "POST":
-        return redirect(url_for("home"))
+        session.permanent = True
+        user = request.form["username"]
+        session["user"] = user
+        return redirect(url_for("login"))
     else:
         return render_template("cadastro.html")
 
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        username = session["user"]
+        return f"<h1>Ola {username}</h1>"
+    else:
+        return redirect(url_for("cadastro"))
+    
+@app.route("/logout")
+def logout():
+    if "user" in session:
+        username = session["user"]
+        flash(f"Usuário, {username}, deslogado com sucesso!", "info")
+    session.pop("user", None)
+    return redirect(url_for("cadastro"))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
